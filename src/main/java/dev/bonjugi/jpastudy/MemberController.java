ab5584241a56e286fb14197ddc0662332b4cb4da
junyoung.kang <junyoung.kang@meshkore.net>
2021-07-25 00:36:34 +0900

    cors 커밋

---
+++ src/main/java/dev/bonjugi/jpastudy/MemberController.java
@@ -0,0 +1,30 @@
+package dev.bonjugi.jpastudy;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.OPTIONS;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+  @CrossOrigin(origins = {
+    "http://localhost:8259",
+    "https://location.dev1.meshdev.io",
+    "https://location.qa1.meshdev.io",
+    "https://location.qa2.meshdev.io",
+    "https://location.qa3.meshdev.io",
+    "https://location.vroong.com"},
+    allowedHeaders = {"X-PINGOTHER", "Content-Type"},
+    methods = {POST, GET, OPTIONS},
+    maxAge = 86400
+    )
+  @GetMapping("/hello")
+  public String hello() {
+
+    return "hello";
+  }
+}
---
+++ src/test/java/dev/bonjugi/jpastudy/member/entity/Hello.java
@@ -0,0 +1,14 @@
+package dev.bonjugi.jpastudy.member.entity;
+
+class Result {
+
+}
+
+public class Hello {
+
+  public static void main(String[] args) {
+    Result result = new Result();
+
+  }
+
+}
